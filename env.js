4515f3d723e5d0e744ac668106e7f4ae19b6f5bb
Siddharth6511 <vashishthatushar004@gmail.com>
2025-08-13 15:14:46 +0530

    middle ware

--- env.js
+++ env.js
@@ -14,26 +14,47 @@
 // app.listen(process.env.PORT)
 
 
-const express  = require('express');
-require('dotenv').config();
+// const express  = require('express');
+// require('dotenv').config();
+// const app = express();
+// const path = require('path') 
+
+// app.get('/', function(req, res){
+//     res.send('<h1>This is home page<h1/>')
+// })
+
+// app.get('/about', function(req, res){
+//     res.sendFile(path.join(__dirname, 'Html', 'index.html'))
+// })
+// app.get('/project', function(req, res){
+//     res.sendFile(path.join(__dirname, 'Html', 'index2.html'))
+// })
+
+// app.get('/contact', function(req, res){
+//     res.redirect(301, '/about')
+//     // 301 indicates a webpage or resource has been permanently moved to a new URL
+//     // 302 mean a resourse has been temp moved to a new URL
+// })
+
+// app.listen(process.env.PORT);
+
+
+
+
+//middleware
+
+const express = require('express');
 const app = express();
-const path = require('path')
+require('dotenv').config();
 
-app.get('/', function(req, res){
-    res.send('<h1>This is home page<h1/>')
+app.get('/', (req, res, next)=>{
+    console.log("This is our home page");
+    next();
 })
 
-app.get('/about', function(req, res){
-    res.sendFile(path.join(__dirname, 'Html', 'index.html'))
-})
-app.get('/project', function(req, res){
-    res.sendFile(path.join(__dirname, 'Html', 'index2.html'))
+app.get('/', (req, res) => {
+    console.log("This is some more information");
+    res.send("Middleware executed")
 })
 
-app.get('/contact', function(req, res){
-    res.redirect(301, '/about')
-    // 301 indicates a webpage or resource has been permanently moved to a new URL
-    // 302 mean a resourse has been temp moved to a new URL
-})
-
-app.listen(process.env.PORT);
+app.listen(process.env.PORT)
