8f568632bdf7ddbe229205b86ef4418a2fea5644
Siddharth6511 <vashishthatushar004@gmail.com>
2025-09-15 21:11:32 +0530

    gg

--- Routes/main.js
+++ Routes/main.js
@@ -11,7 +11,7 @@ app.get("/about", (req, res) =>   {
     res.send("About page")
 })
 
-app.get("/blog/:blogid", ( req, res ) => {
+app.get("/blog/:blogid", ( req , res ) => {
     res.send(req.params.blogid)  //that's how we access params
 })
 
